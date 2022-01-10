643934e0881ff1b1f388a9ebd81450a903804274
prado <antonio.ti.celso@gmail.com>
2022-01-10 01:14:23 +0000

    Creat: Criaçao de class - BeerDTO

---
+++ src/main/java/com/github/antoniocelso/beerstock/dto/BeerDTO.java
@@ -0,0 +1,43 @@
+package com.github.antoniocelso.beerstock.dto;
+
+import com.github.antoniocelso.beerstock.enums.BeerType;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String name;
+
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String brand;
+
+    @NotNull
+    @Max(500)
+    private Integer max;
+
+    @NotNull
+    @Max(100)
+    private Integer quantity;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private BeerType type;
+
+}
