a52b683d3a6d4c1b0a8b1a439e8aa15f52fdbddd
prado <antonio.ti.celso@gmail.com>
2022-01-10 13:04:31 +0000

    Creat: Criaçao de class - BeerRepository

---
+++ src/main/java/com/github/antoniocelso/beerstock/repository/BeerRepository.java
@@ -0,0 +1,12 @@
+package com.github.antoniocelso.beerstock.repository;
+
+import com.github.antoniocelso.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+
+}
