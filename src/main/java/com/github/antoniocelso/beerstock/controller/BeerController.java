799b7ae0beaa3564f9a91524e3a4c91d59cc3979
prado <antonio.ti.celso@gmail.com>
2022-01-10 13:04:12 +0000

    Creat: Criaçao de class - BeerControllerDocs, BeerController

---
+++ src/main/java/com/github/antoniocelso/beerstock/controller/BeerController.java
@@ -0,0 +1,54 @@
+package com.github.antoniocelso.beerstock.controller;
+
+import com.github.antoniocelso.beerstock.dto.BeerDTO;
+import com.github.antoniocelso.beerstock.dto.QuantityDTO;
+import com.github.antoniocelso.beerstock.exception.BeerAlreadyRegisteredException;
+import com.github.antoniocelso.beerstock.exception.BeerNotFoundException;
+import com.github.antoniocelso.beerstock.exception.BeerStockExceededException;
+import com.github.antoniocelso.beerstock.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+public class BeerController  implements BeerControllerDocs{
+
+    private final BeerService beerService;
+
+    @Autowired
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+
+}
